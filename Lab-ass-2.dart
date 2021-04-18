a53bc158c01903b4516ee1ef2e1ba23648cddedc
layon-jin <58416313+layon-jin@users.noreply.github.com>
2021-04-18 13:34:19 -0700

    Update Lab-ass-2.dart

--- Lab-ass-2.dart
+++ Lab-ass-2.dart
@@ -3,8 +3,8 @@ void main() {
   int b = 15;
   int sum = a + b;
   if (sum < 100) {
-    print("its true that the sum of both nums is less than 100");
+    print("true.  the sum of both nums is less than 100");
   } else {
-    print("nah, sum of both  numbers is more than 100");
+    print("false! sum of both  numbers is more than 100");
   }
 }
